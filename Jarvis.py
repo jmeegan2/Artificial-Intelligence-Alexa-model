9a4bf5d539922d81d1c5b959ae6b6dcfaf4347c1
James Meegan <jmeegan2@owlmail.harford.edu>
2022-04-07 17:13:17 -0400

    listening comman is working, reading and speech

--- Jarvis.py
+++ Jarvis.py
@@ -1,11 +1,20 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[11].id)
+engine.say("I am your servant")
+engine.say("What can I do for you master ")
+engine.runAndWait()
 try:
     with sr.Microphone() as source:
-        print('listening')
+        print('listening...')
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
-        print(command)
+        command = command.lower()
+        if 'alexa' in command:
+            print(command)
 except:
     pass
